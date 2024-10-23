efe58319236be55fd0b75ca8d8bd94a33969bb6a
Manasa-katta <kattamanasa1911@gmail.com>
2024-10-23 11:25:21 +0530

    third project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="../images/trollface.webp" alt="trollface" className="header--image"></img>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React - Project 3</h4>
+        </header>
+    )
+}
